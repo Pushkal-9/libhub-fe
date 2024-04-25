1d04335e81893497abe87632c94c9c9eec053495
pushkal <mudhapakapushkal@gmail.com>
2024-04-25 11:53:53 -0400

    endpoint change

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://libhub.us-east-2.elasticbeanstalk.com/',
+    baseURL: 'https://libhub.us-east-2.elasticbeanstalk.com',
 });
 
 export default api;
